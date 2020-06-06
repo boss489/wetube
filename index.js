6ec2381866f13a184acea8c22c8d97b2bc9025c6
boss <boss489@cj.com>
2020-06-07 02:40:39 +0900

    second

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi');
+
+
+const express = require('express');
+const app = express();
+
+const port = 4000;
+function handleListening(){
+    console.log('listening in port ${port}');
+}
+
+app.listen(port, handleListening);
