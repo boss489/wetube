f380cde3c08a31a0def22350bd0f8bd4cf304d75
boss <boss489@cj.com>
2020-07-18 00:13:52 +0900

    test2 new file feature/login

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-
+import express from "express"
 
 const express = require('express');
 const app = express();
 
 const port = 4000;
-function handleListening(){
-    console.log('listening in port ${port}');
-}
+const handleListening = () => 
+console.log('listening in port ${port}');
+// function handleListening(){
+//     console.log('listening in port ${port}');
+// }
+
+const handleHome = (req, res) => res.send("hello from home");
+// function handleHome(req, res){
+//     console.log(req);
+//     res.send('hello from home');
+// }
+
+//arrow function 
+const handleProfile = (req, res) => res.send("you are on my profile");
+// function handleProfile(req, res){
+//     res.send('you are on my profile');
+// }
+const betweenHome = (req,res,next) => {
+    console.log('between');
+    next();
+};
+
+app.use(betweenHome);// 미들웨어로 사용하겠단 뜻
+
+
+app.get("/profile", handleProfile);
+
+app.get("/", betweenHome, handleHome);
 
 app.listen(port, handleListening);
